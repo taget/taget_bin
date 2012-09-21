55745bb4c7ca3760e8601ee3ac6a29f617c0a61a
Eli Qiao <taget@linux.vnet.ibm.com>
2012-09-21 10:34:18 +0800

    Modify Makefile ARG
    Signed-off-by: Eli Qiao <taget@linux.vnet.ibm.com>

--- net/util.h
+++ net/util.h
@@ -4,6 +4,6 @@ using namespace std;
 #ifndef __UTIL_H
 #define __UTIL_H
 
-#define DEBUG(x) cout<<"[DEBUG] "<< __FILE__ << ":"<<__LINE__ <<" : " << #x << " : "<<x<<endl;
+#define DEBUG(x) cout<<"[DEBUG] "<< __FILE__ << ":"<<__LINE__ <<" : " << (#x) << " : "<<(x)<<endl;
 
 #endif
